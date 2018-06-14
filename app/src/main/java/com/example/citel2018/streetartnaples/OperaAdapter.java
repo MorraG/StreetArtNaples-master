36e1749f94580cfbf56567dfcb73460000d11cba
MorraG <guidomorra13@gmail.com>
2018-06-14 23:45:19 +0200

    Adapter and ListView war

--- app/src/main/java/com/example/citel2018/streetartnaples/OperaAdapter.java
+++ app/src/main/java/com/example/citel2018/streetartnaples/OperaAdapter.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.widget.ArrayAdapter;
-import android.content.Context;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,14 +18,6 @@ public class OperaAdapter extends ArrayAdapter<Opera> {
 
     private ArrayList<Opera> dataSet;
     Context mContext;
-
-
-    /**
-     * Create a new OperaAdapter object
-     *
-     * @param
-     * @param opere
-     */
 
     public OperaAdapter(Context context, ArrayList<Opera> opere) {
 
@@ -69,33 +59,39 @@ public class OperaAdapter extends ArrayAdapter<Opera> {
 
         // Find the TextView in the list_item.xml layout with the ID autore_text_view.
         TextView autoreTextView = (TextView) listItemView.findViewById(R.id.autore_text_view);
+
         // Get the autore from the currentOpera object and set this text on
         // the autore TextView.
         autoreTextView.setText(opera.getStreetAuthorId());
 
         // Find the TextView in the list_item.xml layout with the ID titolo_text_view.
         TextView titoloTextView = (TextView) listItemView.findViewById(R.id.titolo_text_view);
+
         // Get the titolo from the currentOpera object and set this text on
         // the default TextView.
         titoloTextView.setText(opera.getStreetTitleId());
 
         // Find the TextView in the list_item.xml layout with the ID address_text_view.
         TextView addressTextView = (TextView) listItemView.findViewById(R.id.default_address_text_view1);
+
         // Get the address from the currentOpera object and set this text on
         // the default TextView.
         addressTextView.setText(opera.getStreetAddressId());
 
         // Find the TextView in the list_item.xml layout with the ID description_text_view.
         TextView descriptionTextView = (TextView) listItemView.findViewById(R.id.default_description_text_view2);
+
         // Get the description from the currentOpera object and set this text on
         // the default TextView.
         descriptionTextView.setText(opera.getStreetDescriptionId());
 
         // Find the ImageView in the list_item.xml layout with the ID image.
         ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+
         // Get the image from the currentOpera object and set this text on
         // the default imageview.
         imageView.setImageResource(opera.getStreetImageResourceId());
+
         return listItemView;
     }
 }
